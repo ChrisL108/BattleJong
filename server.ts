11f61947db712cfa8a5a74fdc84af594862115c9
Chris Lacaille <chrislacaille5@gmail.com>
2022-09-07 08:15:11 -0500

    add websocket example

--- server.ts
+++ server.ts
@@ -1,5 +1,14 @@
 import path from 'path'
 import express, { Express } from 'express'
-import WS from 'ws'
+import WebSocket from 'ws'
 
 const app = express()
+
+const wsServer = new WebSocket.Server({ port: 8080 })
+
+wsServer.on('connection', (socket: WebSocket, req) => {
+  socket.on('message', (inMsg) => {
+    console.log('inMsg: ', inMsg)
+    socket.send(inMsg)
+  })
+})
